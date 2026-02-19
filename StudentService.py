53a10986234c06d3054b5172be085abfc8dd540b
u.martingoulet <ulricmartingoulet@gmail.com>
2026-02-19 11:22:13 +0100

    student service

---
+++ StudentService.py
@@ -0,0 +1,13 @@
+class StudentService:
+
+    def printList(self) -> None:
+        """Affiche la liste des étudiants"""
+        pass
+
+    def addStudent(self, student: 'Student') -> None:
+        """Ajoute un étudiant à la liste"""
+        pass
+
+    def removeStudent(self, student_id: int) -> None:
+        """Supprime un étudiant par son id"""
+        pass
